9e7d1e7ae7dd79b52ca62119be53d96ac1c3e64e
kenny.lim <kenny.lim@daumkakao.com>
2017-03-31 22:09:32 +0900

    perspective_trasform class added

--- perspective_trasform.py
+++ perspective_trasform.py
@@ -1,30 +1,37 @@
 import cv2
 import numpy as np
 
-def src_dst_rect(img_size):
-    w, h = img_size
+class Transformer:
+    def __init__(self, img_size):
+        self.img_size = img_size
+        self.src, self.dst = self.src_dst_rect(img_size)
 
-    src_top_margin = h // 2 + 80
-    src_upper_left_right_margin = 540
-    src_lower_left_right_margin = 40
+    def src_dst_rect(self, img_size):
+        w, h = img_size
 
-    dst_left_right_margin = 40
+        src_top_margin = h // 2 + 94
+        src_upper_left_right_margin = 570
+        src_lower_left_right_margin = 146
 
-    src = np.float32([[src_upper_left_right_margin, src_top_margin], [w - src_upper_left_right_margin, src_top_margin],
-                      [w - src_lower_left_right_margin, h], [src_lower_left_right_margin, h]])
+        dst_left_right_margin = 160
 
-    dst = np.float32([[dst_left_right_margin, 0], [w - dst_left_right_margin, 0],
-                      [w - dst_left_right_margin, h], [dst_left_right_margin, h]])
+        src = np.float32([[src_upper_left_right_margin, src_top_margin], [w - src_upper_left_right_margin, src_top_margin],
+                          [w - src_lower_left_right_margin, h], [src_lower_left_right_margin, h]])
 
-    return src, dst
+        dst = np.float32([[dst_left_right_margin, 0], [w - dst_left_right_margin, 0],
+                          [w - dst_left_right_margin, h], [dst_left_right_margin, h]])
 
-def warp(img):
-    img_size = (img.shape[1], img.shape[0])
-    src, dst = src_dst_rect(img_size)
-    M = cv2.getPerspectiveTransform(src, dst)
-    Minv = cv2.getPerspectiveTransform(dst, src)
-    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+        return src, dst
 
-    return warped, M, Minv
+    def warp(self, img):
+        img_size = (img.shape[1], img.shape[0])
+        M = cv2.getPerspectiveTransform(self.src, self.dst)
+        warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
 
+        return warped
 
+    def unwarp(self, img):
+        img_size = (img.shape[1], img.shape[0])
+        Minv = cv2.getPerspectiveTransform(self.dst, self.src)
+        unwarped = cv2.warpPerspective(img, Minv, img_size, flags=cv2.INTER_LINEAR)
+        return unwarped
